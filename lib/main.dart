efd6df98ce7396a0822e094af5c637225c481975
Dospacite <dospacite@protonmail.com>
2022-12-01 15:06:33 +0300

    Added nose selection and location selection screen.

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,36 @@ class SignLanguagerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: GenderSelectScreen(),
+      home: LocationSelectScreen(),
     );
   }
 }
+
+class LocationSelectScreen extends StatelessWidget {
+  const LocationSelectScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Row(
+
+              ),
+              Row(
+
+              ),
+            ],
+          ),
+          appBar: AppBar(
+
+          ),
+        )
+    );
+  }
+}
+
 
 class GenderSelectScreen extends StatelessWidget {
   const GenderSelectScreen({super.key});
@@ -194,18 +220,84 @@ class HeadView extends StatelessWidget {
     return Stack(
       children: [
         Image.network("https://www.easydrawingtips.com/wp-content/uploads/2018/05/female_face_drawing.png"),
-        SizedBox(
-          width: 50,
-          height: 50,
-          child: MaterialButton(
-            onPressed: () => Navigator.push(
+        Positioned(
+          left: 170,
+          top: 165,
+          child: SizedBox(
+            width: 50,
+            height: 35,
+            child: MaterialButton(
+              color: Colors.black12,
+              onPressed: () => Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => const HeadSelection())),
+                  builder: (context) => const NoseSelection())),
+              ),
+            ),
           ),
-        ),
       ]
     );
   }
+}
 
+class NoseSelection extends StatelessWidget {
+  const NoseSelection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+                Container(
+                  color: Colors.purple,
+                  child: const Text("Bruh"),
+              ),
+              Expanded(
+                flex: 8,
+                child: GridView.count(
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[100],
+                      child: const Text("He'd have you all unravel at the"),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[200],
+                      child: const Text('Heed not the rabble'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[300],
+                      child: const Text('Sound of screams but the'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[400],
+                      child: const Text('Who scream'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[500],
+                      child: const Text('Revolution is coming...'),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      color: Colors.teal[600],
+                      child: const Text('Revolution, they...'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+    );
+  }
 }
